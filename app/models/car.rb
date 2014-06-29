a5846cbe85d9afb77b4d141cab8768183590d6c5
TeamRory <rorycteam@gmail.com>
2014-06-29 18:23:01 -0400

    set up routes and models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,8 @@
 class Car < ActiveRecord::Base
+  belongs_to :manufacturer
+  validates :color, presence: true
+  validates :mileage, presence: true
+  validates :manufacturer_id, presence: true
+  validates :year, presence: true,
+    numericality: {greater_than: 1919, less_than_or_equal_to: Time.now.year}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+root to: 'manufacturer#index'
+
+resources :manufacturers do
+  resources :cars
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
